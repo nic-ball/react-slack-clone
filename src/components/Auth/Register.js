f70f5e0b37a071344c8be88fe76f5bc831835470
nic-ball <nicholasball147@gmail.com>
2020-11-13 12:18:42 +0000

    Created Register page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class Register extends Component {
+  state ={}
+
+  handleChange = () => {}
+
   render() {
     return (
-      <div>Register</div>
-    )
+      <Grid textAlign="center" verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: 450}}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for devChat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} type="text" />
+
+              <Form.Input fluid name="email" icon="user" iconPosition="left" placeholder="email" onChange={this.handleChange} type="email" />
+
+              <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} type="password" />
+
+              <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+
+              <Button color="orange" fluid size="large">Submit</Button>
+            </Segment>
+          </Form>
+          <Message>Aready a user? <Link to="/login">Login</Link></Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
